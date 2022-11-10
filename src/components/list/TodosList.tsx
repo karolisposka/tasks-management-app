7110a3af15357afef0cc407cc916aab6628ce483
karolisposka <karolisposka96@gmail.com>
2022-11-10 17:52:10 +0200

    listCard element created. Edit, delete, done features implemented

--- src/components/list/TodosList.tsx
+++ src/components/list/TodosList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Todo} from '../../model';
 import './TodosList.css';
+import ListCard from '../listCard/ListCard';
 
 type Props = {
   todos: Todo[],
@@ -11,7 +12,7 @@ const TodosList = ({todos, setTodos}: Props) => {
   return (
     <div className='list'>
       {todos.map(todo => (
-        <div key={todo.id}> {todo.todo} </div>
+        <ListCard key={todo.id} todo={todo} todos={todos} setTodos={setTodos}/>
       ))}
     </div>
   )
--- src/components/listCard/ListCard.tsx
+++ src/components/listCard/ListCard.tsx
@@ -1,16 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {AiFillEdit, AiFillDelete} from 'react-icons/ai';
+import {Todo} from '../../model';
+import {MdDone} from 'react-icons/md';
 import './ListCard.css';
 
 type Props = {
-    todo: string,
-    // handleEdit: ,
-    // handleDelete: ,
-    // handleSetCompleted: ,
+    todo: Todo,
+    todos: Todo[],
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>,
 }
 
-const ListCard = ({todo}: Props) => {
-  return (
-    <div className='card'>ListCard</div>
+const ListCard = ({todo, todos, setTodos}: Props) => {
+    const [edit, setEdit] = useState<boolean>(false);
+    const [input, setInput] = useState<string>(todo.todo);
+
+    const handleDone = (id: number) => {
+        setTodos(todos.map(todo => todo.id === id ? {...todo, isDone: !todo.isDone} : todo))
+    }
+
+    const handleDelete = (id: number) => {
+        setTodos(todos.filter(todo => todo.id !== id));
+    }
+
+    const handleEdit = () => {
+        setEdit(!edit);
+    }
+
+    return(
+    <div className='card'>
+        {todo.isDone 
+        ?
+        <s className='card_text'>{todo.todo}</s>
+        :
+        !todo.isDone && edit ?
+        <input/>
+        :
+        <span className='card_text'>{todo.todo}</span>
+        }
+        <div className='card_buttons_wrapper'>
+            <span className='card_button' onClick={()=>{
+                handleEdit();
+            }}>
+                <AiFillEdit/>
+            </span>
+            <span className='card_button' onClick={()=>{
+                handleDelete(todo.id)
+            }}>
+                <AiFillDelete/>
+            </span>
+            <span className='card_button' onClick={()=>{
+                handleDone(todo.id)}}>
+                <MdDone/>
+            </span>
+        </div>
+    </div>
   )
 }
 
